b61979b75ae65361effb2d8a2ff3377ee34ad837
lixizheng <lixizheng@jcmsc.cn>
2023-06-14 20:12:49 +0800

    层序遍历二叉树

--- src/com/algorithm/topic/LevelOrder.java
+++ src/com/algorithm/topic/LevelOrder.java
@@ -3,6 +3,7 @@ package com.algorithm.topic;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -35,6 +36,50 @@ public class LevelOrder {
         int[] res = new int[list.size()];
         for (int i = 0; i < res.length; i++) {
             res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    public List<List<Integer>> leveOder2(TreeNode root){
+        if(root == null) return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        List<List<Integer>> list = new ArrayList<>();
+        while (!queue.isEmpty()){
+            List<Integer> arrayList = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--){
+                TreeNode poll = queue.poll();
+                if(poll != null) {
+                    arrayList.add(poll.val);
+                    if (poll.left != null) queue.add(poll.left);
+                    if (poll.right != null) queue.add(poll.right);
+                }
+            }
+            list.add(arrayList);
+        }
+        return list;
+    }
+
+    public List<List<Integer>> leveOder3(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                assert node != null;
+                if(res.size() % 2 == 0) {
+                    tmp.addLast(node.val);
+                }
+                else {
+                    tmp.addFirst(node.val);
+                }
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
         }
         return res;
     }
